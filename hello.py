20d32754437d560dc4ecaf5f427bc89d86b0bb9f
apros77 <109696484+apros77@users.noreply.github.com>
2023-02-06 09:56:27 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,60 @@
+import appdaemon.plugins.hass.hassapi as hass
+#import cv2
+
+import os
+import pathlib
+import time
+
+
+class SingleDeformer:
+
+    def getmesh(self, img):
+        #Map a target rectangle onto a source quad
+        return [(
+                # target rectangle
+                (0, 0, 730, 320),
+                # corresponding source quadrilateral
+                (50, 30, 10, 290, 715, 290, 670, 25)
+                )]
+
+
+filepath = os.path.realpath(__file__)
+dirpath = os.getcwd()
+open("/config/www/auer/readme.txt", "w")
+
+from PIL import Image, ImageOps
+from PIL import Image, ImageFilter
+# im = Image.open("/config/autosnap.png")
+# for i in range(36):
+im = Image.open("/config/www/auer/lcd_0.png")
+   # im = im.rotate(i * 10)
+   
+left = 300
+top = 180
+right = 1030
+bottom = 500
+ 
+# Cropped image of above dimension
+# (It will not change original image)
+imcrop = im.crop((left, top, right, bottom))  
+imcrop.save("/config/www/auer/lcd_crop_0.png")
+#edges = im.filter(ImageFilter.FIND_EDGES)
+# time.sleep(15)
+#edges.save("/config/www/auer/lcd_edge_0.png")
+result_image = ImageOps.deform(imcrop, SingleDeformer())
+result_image.save("/config/www/auer/imageops-deform.png")
+
+
+#
+# Hellow World App
+#
+# Args:
+#
+
+class HelloWorld(hass.Hass):
+
+  def initialize(self):
+     self.log("$$$$$$$$$$$$$$$$Hello from AppDaemon")
+     self.log("$$$$$$$$$$$$$$$$You are now ready to run Apps!")
+     self.log("*****  " + dirpath)
+     self.log("@@@@. " + filepath)
